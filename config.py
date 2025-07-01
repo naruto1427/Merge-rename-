6adae1a4dbaa40e8a8e0fffb5e6369d9d53ba54a
naruto1427 <pritnaruto@gmail.com>
2025-07-01 16:29:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,19 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-
+    #merge variables 
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    TELEGRAM_API = os.environ["TELEGRAM_API"]
+    OWNER = os.environ.get("OWNER")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    PASSWORD = os.environ.get("PASSWORD")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING")
+    IS_PREMIUM = False
+    MODES = ["video-video", "video-audio", "video-subtitle","extract-streams"]
 
 
 class Txt(object):
